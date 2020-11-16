b25d2afd810b3720c2d74cf1072a0f8e05863458
TymanWasTaken <32660892+TymanWasTaken@users.noreply.github.com>
2020-11-16 16:11:53 -0700

    Add another converter/check to list joining

--- postbin/__init__.py
+++ postbin/__init__.py
@@ -115,6 +115,10 @@ def postSync(content: typing.Union[str, list], *, url: str = None, retry: int = 
     if not requests:
         raise RuntimeError("requests must be installed if you want to be able to run postSync.")
     if isinstance(content, list):
+        try:
+            content = [str(item) for item in content]
+        except:
+            raise TypeError("Unable to convert " + type(content).__name__ + " to str.")
         content = "\n".join(content)
     if not isinstance(content, str):
         raise TypeError("Content parameter should be list or string, not " + type(content).__name__)
@@ -150,6 +154,10 @@ async def postAsync(content: typing.Union[str, list], *, url: str = None, retry:
     if not aiohttp:
         raise RuntimeError("aiohttp must be installed if you want to be able to run postAsync.")
     if isinstance(content, list):
+        try:
+            content = [str(item) for item in content]
+        except:
+            raise TypeError("Unable to convert " + type(content).__name__ + " to str.")
         content = "\n".join(content)
     if not isinstance(content, str):
         raise TypeError("Content parameter should be list or string, not " + type(content).__name__)
