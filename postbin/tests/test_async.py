4bc726e3de8d687ea3e41f73730ba2e2b7a06ae2
dragdev <53234499+EEKIM10@users.noreply.github.com>
2020-11-17 17:57:16 +0000

    Fix pylint more

--- postbin/tests/test_async.py
+++ postbin/tests/test_async.py
@@ -5,18 +5,24 @@ def test_async():
     loop = get_event_loop()
     try:
         assert loop.run_until_complete(
-            postAsync("TestSync on repo https://github.com/dragdev-studios/postbin - Please ignore")
-        ).startswith("https://")
+            postAsync("TestSync on repo https://github.com/dragdev-studios/postbin - Please ignore",
+                      url="http://51.195.138.2:8888")
+        ).startswith("http")
     except Exception as e:
         raise AssertionError from e
 
-# def test_sync_failure():
-#     loop = get_event_loop()
-#     try:
-#         result = loop.run_until_complete(
-#             postAsync("TestAsync on repo https://github.com/dragdev-studios/postbin - Please ignore")
-#         )
-#     except:
-#         assert True
-#     else:
-#         assert not result.startswith("http")
+def test_sync_failure():
+    class NonStringable(object):
+        def __str__(self):
+            raise NotImplementedError
+        def __repr__(self):
+            raise NotImplementedError
+    loop = get_event_loop()
+    try:
+        result = loop.run_until_complete(
+            postAsync(NonStringable(), url="http://51.195.138.2:8888")
+        )
+    except:
+        assert True
+    else:
+        assert not result.startswith("http")
--- postbin/tests/test_sync.py
+++ postbin/tests/test_sync.py
@@ -2,15 +2,21 @@ from postbin import postSync
 
 def test_sync():
     try:
-        result = postSync("TestSync on repo https://github.com/dragdev-studios/postbin - Please ignore")
+        result = postSync("TestSync on repo https://github.com/dragdev-studios/postbin - Please ignore",
+                          url="http://51.195.138.2:8888")
     except Exception as e:
         raise AssertionError from e
-    assert result.startswith("https://")
+    assert result.startswith("http")
 
-# def test_sync_failure():
-#     try:
-#         result = postSync("", url="http://example.com")  # sorry example.com
-#     except:
-#         return True
-#     else:
-#         assert not result.startswith("http")
+def test_sync_failure():
+    class NonStringable(object):
+        def __str__(self):
+            raise NotImplementedError
+        def __repr__(self):
+            raise NotImplementedError
+    try:
+        result = postSync(NonStringable(), url="http://51.195.138.2:8888")
+    except:
+        return True
+    else:
+        assert not result.startswith("http")
