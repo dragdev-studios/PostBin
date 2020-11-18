b04c10f8a86148786ed3a6c0d01c16c37add0880
dragdev <53234499+EEKIM10@users.noreply.github.com>
2020-11-18 17:37:12 +0000

    Add tests for v2

---
+++ postbin/v2/tests/test_async_v2.py
@@ -0,0 +1,32 @@
+from .. import AsyncHaste, ConfigOptions
+from asyncio import get_event_loop
+
+loop = get_event_loop()
+
+def post_test():
+    from random import shuffle
+    chars = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    shuffle(chars)
+
+    try:
+        cls = AsyncHaste(''.join(chars),verbose=0)
+    except:
+        raise AssertionError
+    else:
+        result = loop.run_until_complete(cls.post())
+        assert result.startswith("http")
+
+def get_test():
+    from random import shuffle
+    chars = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    shuffle(chars)
+
+    try:
+        cls = AsyncHaste(''.join(chars),verbose=0)
+    except:
+        raise AssertionError
+    else:
+        result = loop.run_until_complete(cls.post(config=ConfigOptions(return_full_url=False)))
+        assert result.startswith("http")
+        result2 = loop.run_until_complete(cls.raw(result))
+        assert result is not None and result == chars
