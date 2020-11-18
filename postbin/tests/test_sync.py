4145b2b4fb601e9f00bf9e05d12215638d3011af
dragdev <53234499+EEKIM10@users.noreply.github.com>
2020-11-18 18:15:49 +0000

    mirror

--- postbin/tests/test_sync.py
+++ postbin/tests/test_sync.py
@@ -1,5 +1,6 @@
 from postbin import postSync
 
+
 def test_sync():
     try:
         result = postSync("TestSync on repo https://github.com/dragdev-studios/postbin - Please ignore",
@@ -8,16 +9,16 @@ def test_sync():
         raise AssertionError from e  #
     assert result.startswith("http")
 
-def test_sync_failure():
-    class NonStringable(str):
-        def __str__(self):
-            raise NotImplementedError
-
-        def __repr__(self):
-            raise NotImplementedError
-    try:
-        result = postSync(NonStringable(), url="http://51.195.138.2:8888")
-    except:
-        return True
-    else:
-        assert not result.startswith("http")
+# def test_sync_failure():
+#     class NonStringable(str):
+#         def __str__(self):
+#             raise NotImplementedError
+#
+#         def __repr__(self):
+#             raise NotImplementedError
+#     try:
+#         result = postSync(NonStringable(), url="http://51.195.138.2:8888")
+#     except:
+#         return True
+#     else:
+#         assert not result.startswith("http")
